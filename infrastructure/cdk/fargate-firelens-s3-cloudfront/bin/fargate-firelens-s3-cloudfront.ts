10537daf092363526bda7fc166b881f99e88b955
Toshifumi Kurosawa <kuromailserver@gmail.com>
2025-01-18 23:16:30 +0900

    Refactor ECS configuration and streamline project structure
    - Updated environment variable names for clarity and consistency across multiple files.
    - Consolidated health check and logging endpoints for improved organization.
    - Removed obsolete CDK ECS Firelens sample project files to clean up the codebase.
    - Updated ECS stack identifier and environment variables to reflect the new version and server address.
    - Enhanced API error handling and logging for better monitoring and debugging.

---
+++ infrastructure/cdk/fargate-firelens-s3-cloudfront/bin/fargate-firelens-s3-cloudfront.ts
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+import * as cdk from 'aws-cdk-lib';
+import { FargateFirelensS3CloudfrontStack } from '../lib/fargate-firelens-s3-cloudfront-stack';
+
+const app = new cdk.App();
+new FargateFirelensS3CloudfrontStack(app, "cdk-faragate-01", {
+  env: {
+    account: "985539793438",
+    region: "ap-northeast-1",
+  },
+});
---
+++ infrastructure/cdk/fargate-firelens-s3-cloudfront/test/fargate-firelens-s3-cloudfront.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as FargateFirelensS3Cloudfront from '../lib/fargate-firelens-s3-cloudfront-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/fargate-firelens-s3-cloudfront-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new FargateFirelensS3Cloudfront.FargateFirelensS3CloudfrontStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
