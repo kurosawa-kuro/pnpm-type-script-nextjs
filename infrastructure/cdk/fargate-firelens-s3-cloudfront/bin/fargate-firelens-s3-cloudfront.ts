eca804854a61936af08d7f33ef1136d22266a49e
Toshifumi Kurosawa <kuromailserver@gmail.com>
2025-01-19 07:46:00 +0900

    Refactor Fargate stack to enhance configuration and resource management
    - Introduced a centralized configuration object to streamline stack setup and improve readability.
    - Refactored the FargateFirelensS3CloudfrontStack to utilize the new configuration structure, enhancing modularity.
    - Created dedicated methods for network, storage, IAM, and application resource creation, improving code organization.
    - Updated security group rules to allow application traffic and refined IAM role permissions for better security.
    - Enhanced CloudFront distribution setup and output generation for improved clarity and maintainability.

--- infrastructure/cdk/fargate-firelens-s3-cloudfront/bin/fargate-firelens-s3-cloudfront.ts
+++ infrastructure/cdk/fargate-firelens-s3-cloudfront/bin/fargate-firelens-s3-cloudfront.ts
@@ -3,15 +3,25 @@ import * as cdk from 'aws-cdk-lib';
 import { FargateFirelensS3CloudfrontStack } from '../lib/fargate-firelens-s3-cloudfront-stack';
 import { CommonResourceStack } from '../lib/common-resource-stack';
 
+const CONFIG = {
+  prefix: 'cdkfargate01',
+  vpcCidr: '10.0.0.0/16',
+  appPort: 3000,
+  containerConfig: {
+    cpu: 512,
+    memoryLimitMiB: 1024,
+    firelensMemoryMiB: 50,
+    appMemoryMiB: 256,
+  }
+};
+
 const app = new cdk.App();
 
-const PREFIX = 'cdkfargate01';
-
 const commonResources = new CommonResourceStack(app, 'CommonResourceStack', {
-  prefix: PREFIX,
+  prefix: CONFIG.prefix,
 });
 
 new FargateFirelensS3CloudfrontStack(app, 'FargateFirelensS3CloudfrontStack', {
-  prefix: PREFIX,
+  config: CONFIG,
   commonResources,
 });
--- infrastructure/cdk/fargate-firelens-s3-cloudfront/lib/fargate-firelens-s3-cloudfront-stack.ts
+++ infrastructure/cdk/fargate-firelens-s3-cloudfront/lib/fargate-firelens-s3-cloudfront-stack.ts
@@ -4,130 +4,245 @@ import {
   aws_s3_assets as assets,
   aws_cloudfront as cloudfront,
   aws_cloudfront_origins as origins,
+  aws_ec2 as ec2,
+  aws_iam as iam,
+  aws_s3 as s3,
   Stack,
   StackProps,
   CfnOutput,
+  RemovalPolicy,
 } from "aws-cdk-lib";
 import { FirelensLogRouterType } from "aws-cdk-lib/aws-ecs";
 import * as path from "path";
 import * as firehose from "@aws-cdk/aws-kinesisfirehose-alpha";
 import * as destinations from "@aws-cdk/aws-kinesisfirehose-destinations-alpha";
 import * as ecs_patterns from "aws-cdk-lib/aws-ecs-patterns";
-import { CommonResourceStack, getResourceName } from './common-resource-stack';
+import { CommonResourceStack } from "./common-resource-stack";
+
+// スタックの設定インターフェース
+interface StackConfig {
+  prefix: string;
+  vpcCidr: string;
+  containerConfig: {
+    cpu: number;
+    memoryLimitMiB: number;
+    firelensMemoryMiB: number;
+    appMemoryMiB: number;
+  };
+}
 
 interface FargateFirelensS3CloudfrontStackProps extends StackProps {
-  prefix: string;
+  config: {
+    prefix: string;
+    vpcCidr: string;
+    appPort: number;
+    containerConfig: {
+      cpu: number;
+      memoryLimitMiB: number;
+      firelensMemoryMiB: number;
+      appMemoryMiB: number;
+    }
+  };
   commonResources: CommonResourceStack;
 }
 
 export class FargateFirelensS3CloudfrontStack extends Stack {
+  private readonly config: StackConfig;
+  private vpc: ec2.Vpc;
+  private securityGroup: ec2.SecurityGroup;
+  private taskRole: iam.Role;
+  private logBucket: s3.Bucket;
+  private imageBucket: s3.Bucket;
+
   constructor(scope: Construct, id: string, props: FargateFirelensS3CloudfrontStackProps) {
     super(scope, id, props);
 
-    const { vpc, fargateSecurityGroup, taskRole, logBucket, imageBucket } = props.commonResources;
+    // propsから設定を取得
+    this.config = props.config;
 
-    // Assets
-    const asset = new assets.Asset(this, getResourceName(props.prefix, 'asset'), {
+    // リソースの作成
+    this.createNetworkResources();
+    this.createStorageResources();
+    this.createIamResources();
+    this.createApplicationResources();
+  }
+
+  // ネットワークリソースの作成
+  private createNetworkResources(): void {
+    // VPC作成
+    this.vpc = new ec2.Vpc(this, this.getResourceName('vpc'), {
+      vpcName: this.getResourceName('vpc'),
+      ipAddresses: ec2.IpAddresses.cidr(this.config.vpcCidr),
+      maxAzs: 2,
+      natGateways: 0,
+      subnetConfiguration: [{
+        name: 'Public',
+        subnetType: ec2.SubnetType.PUBLIC,
+        mapPublicIpOnLaunch: true,
+        cidrMask: 24
+      }],
+    });
+
+    // セキュリティグループ作成
+    this.securityGroup = new ec2.SecurityGroup(this, this.getResourceName('securityGroup'), {
+      vpc: this.vpc,
+      securityGroupName: this.getResourceName('securityGroup'),
+      description: 'Security group for Fargate containers'
+    });
+
+    this.securityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(80));
+    this.securityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(443));
+    this.securityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(3000), 'Allow application traffic');
+  }
+
+  // ストレージリソースの作成
+  private createStorageResources(): void {
+    this.logBucket = new s3.Bucket(this, this.getResourceName('logBucket'), {
+      removalPolicy: RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+      objectOwnership: s3.ObjectOwnership.BUCKET_OWNER_PREFERRED,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+    });
+
+    this.imageBucket = new s3.Bucket(this, this.getResourceName('imageBucket'), {
+      removalPolicy: RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      cors: [{
+        allowedHeaders: ['*'],
+        allowedMethods: [s3.HttpMethods.PUT, s3.HttpMethods.POST, s3.HttpMethods.GET, s3.HttpMethods.DELETE],
+        allowedOrigins: ['*'],
+        exposedHeaders: [],
+      }],
+    });
+  }
+
+  // IAMリソースの作成
+  private createIamResources(): void {
+    this.taskRole = new iam.Role(this, this.getResourceName('taskRole'), {
+      assumedBy: new iam.ServicePrincipal("ecs-tasks.amazonaws.com"),
+    });
+
+    this.taskRole.addToPolicy(new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW,
+      actions: ['s3:PutObject', 's3:GetObject', 's3:ListBucket'],
+      resources: ['arn:aws:s3:::*/*'],
+    }));
+  }
+
+  // アプリケーションリソースの作成
+  private createApplicationResources(): void {
+    const asset = new assets.Asset(this, this.getResourceName('asset'), {
       path: path.join(__dirname, "extra.conf"),
     });
 
-    // Firehose
-    new firehose.DeliveryStream(this, getResourceName(props.prefix, 'deliveryStream'), {
-      deliveryStreamName: getResourceName(props.prefix, 'deliveryStream'),
-      destination: new destinations.S3Bucket(logBucket),
+    const deliveryStream = new firehose.DeliveryStream(this, this.getResourceName('deliveryStream'), {
+      deliveryStreamName: this.getResourceName('deliveryStream'),
+      destination: new destinations.S3Bucket(this.logBucket),
     });
 
-    // ECS Cluster
-    const cluster = new ecs.Cluster(this, getResourceName(props.prefix, 'cluster'), { vpc });
-
-    // Task Definition
-    const taskDefinition = new ecs.FargateTaskDefinition(
-      this,
-      getResourceName(props.prefix, 'taskDefinition'),
-      {
-        cpu: 512,
-        memoryLimitMiB: 1024,
-        taskRole: taskRole,
-      }
-    );
-
-    // Firelensログルーターコンテナ
-    const firelensLog = taskDefinition.addFirelensLogRouter(
-      getResourceName(props.prefix, 'firelensLog'),
-      {
-        image: ecs.ContainerImage.fromRegistry('amazon/aws-for-fluent-bit:latest'),
-        firelensConfig: {
-          type: FirelensLogRouterType.FLUENTBIT,
-          options: {
-            enableECSLogMetadata: true,
-            configFileValue: asset.s3ObjectUrl,
-          },
-        },
-        logging: ecs.LogDrivers.awsLogs({
-          streamPrefix: getResourceName(props.prefix, 'firelens'),
-        }),
-        memoryReservationMiB: 50,
-      }
-    );
-
-    // アプリケーションコンテナ
-    const appContainer = taskDefinition.addContainer(
-      getResourceName(props.prefix, 'appContainer'),
-      {
-        image: ecs.ContainerImage.fromRegistry('nginx:latest'),
-        logging: ecs.LogDrivers.firelens({
-          options: {
-            Name: 'firehose',
-            region: this.region,
-            delivery_stream: getResourceName(props.prefix, 'deliveryStream'),
-          },
-        }),
-        memoryReservationMiB: 256,
-        portMappings: [{ containerPort: 80 }],
-      }
-    );
-
-    // CloudFront Distribution
-    const distribution = new cloudfront.Distribution(
-      this,
-      getResourceName(props.prefix, 'distribution'),
-      {
-        defaultBehavior: {
-          origin: new origins.S3Origin(imageBucket),
-          allowedMethods: cloudfront.AllowedMethods.ALLOW_ALL,
-          viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
-          cachePolicy: cloudfront.CachePolicy.CACHING_OPTIMIZED,
-        },
-        additionalBehaviors: {
-          '/api/*': {
-            origin: new origins.HttpOrigin(`${getResourceName(props.prefix, 'api')}.${this.region}.amazonaws.com`),
-            allowedMethods: cloudfront.AllowedMethods.ALLOW_ALL,
-            viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
-            cachePolicy: cloudfront.CachePolicy.CACHING_DISABLED,
-          },
-        },
-        priceClass: cloudfront.PriceClass.PRICE_CLASS_200,
-        enableLogging: true,
-        logBucket: logBucket,
-        logFilePrefix: 'cloudfront-logs/',
-      }
-    );
-
+    const cluster = new ecs.Cluster(this, this.getResourceName('cluster'), { vpc: this.vpc });
+    const taskDefinition = this.createTaskDefinition();
+    
     // Fargate Service
     const fargateService = new ecs_patterns.ApplicationLoadBalancedFargateService(
       this,
-      getResourceName(props.prefix, 'fargateService'),
+      this.getResourceName('fargateService'),
       {
         cluster,
-        taskDefinition: taskDefinition,
+        taskDefinition,
         desiredCount: 1,
         assignPublicIp: true,
-        securityGroups: [fargateSecurityGroup],
+        securityGroups: [this.securityGroup],
         publicLoadBalancer: true,
       }
     );
 
-    // Output
+    // CloudFront Distribution
+    const distribution = this.createCloudFrontDistribution();
+
+    // Outputs
+    this.createOutputs(distribution, fargateService);
+  }
+
+  // タスク定義の作成
+  private createTaskDefinition(): ecs.FargateTaskDefinition {
+    const asset = new assets.Asset(this, this.getResourceName('asset'), {
+      path: path.join(__dirname, "extra.conf"),
+    });
+
+    const taskDefinition = new ecs.FargateTaskDefinition(
+      this,
+      this.getResourceName('taskDefinition'),
+      {
+        cpu: this.config.containerConfig.cpu,
+        memoryLimitMiB: this.config.containerConfig.memoryLimitMiB,
+        taskRole: this.taskRole,
+      }
+    );
+
+    // Firelensログルーター
+    taskDefinition.addFirelensLogRouter(this.getResourceName('firelensLog'), {
+      image: ecs.ContainerImage.fromRegistry('amazon/aws-for-fluent-bit:latest'),
+      firelensConfig: {
+        type: FirelensLogRouterType.FLUENTBIT,
+        options: {
+          enableECSLogMetadata: true,
+          configFileValue: asset.s3ObjectUrl,
+        },
+      },
+      logging: ecs.LogDrivers.awsLogs({
+        streamPrefix: this.getResourceName('firelens'),
+      }),
+      memoryReservationMiB: this.config.containerConfig.firelensMemoryMiB,
+    });
+
+    // アプリケーションコンテナ
+    taskDefinition.addContainer(this.getResourceName('appContainer'), {
+      image: ecs.ContainerImage.fromRegistry('nginx:latest'),
+      logging: ecs.LogDrivers.firelens({
+        options: {
+          Name: 'firehose',
+          region: this.region,
+          delivery_stream: this.getResourceName('deliveryStream'),
+        },
+      }),
+      memoryReservationMiB: this.config.containerConfig.appMemoryMiB,
+      portMappings: [{ containerPort: 80 }],
+    });
+
+    return taskDefinition;
+  }
+
+  // CloudFront Distributionの作成
+  private createCloudFrontDistribution(): cloudfront.Distribution {
+    return new cloudfront.Distribution(this, this.getResourceName('distribution'), {
+      defaultBehavior: {
+        origin: new origins.S3Origin(this.imageBucket),
+        allowedMethods: cloudfront.AllowedMethods.ALLOW_ALL,
+        viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+        cachePolicy: cloudfront.CachePolicy.CACHING_OPTIMIZED,
+      },
+      additionalBehaviors: {
+        '/api/*': {
+          origin: new origins.HttpOrigin(`${this.getResourceName('api')}.${this.region}.amazonaws.com`),
+          allowedMethods: cloudfront.AllowedMethods.ALLOW_ALL,
+          viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+          cachePolicy: cloudfront.CachePolicy.CACHING_DISABLED,
+        },
+      },
+      priceClass: cloudfront.PriceClass.PRICE_CLASS_200,
+      enableLogging: true,
+      logBucket: this.logBucket,
+      logFilePrefix: 'cloudfront-logs/',
+    });
+  }
+
+  // 出力の作成
+  private createOutputs(
+    distribution: cloudfront.Distribution,
+    fargateService: ecs_patterns.ApplicationLoadBalancedFargateService
+  ): void {
     new CfnOutput(this, 'DistributionDomainName', {
       value: distribution.distributionDomainName,
       description: 'CloudFront Distribution Domain Name',
@@ -137,5 +252,10 @@ export class FargateFirelensS3CloudfrontStack extends Stack {
       value: fargateService.loadBalancer.loadBalancerDnsName,
       description: 'Application Load Balancer DNS Name',
     });
+  }
+
+  // リソース名の生成
+  private getResourceName(resourceType: string): string {
+    return `${this.config.prefix}${resourceType.charAt(0).toUpperCase()}${resourceType.slice(1)}`;
   }
 }
