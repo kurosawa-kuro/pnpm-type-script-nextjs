da07a6c0ea935f9567d4e9ec9d8ed6de36d1beb7
Toshifumi Kurosawa <kuromailserver@gmail.com>
2025-01-19 06:32:53 +0900

    aaaaaaaaaa

---
+++ fullstack-nextjs/src/types/aws-sdk.d.ts
@@ -0,0 +1,7 @@
+declare module '@aws-sdk/client-s3' {
+    export * from '@aws-sdk/client-s3/dist-types'
+  }
+  
+  declare module '@aws-sdk/s3-request-presigner' {
+    export * from '@aws-sdk/s3-request-presigner/dist-types'
+  }
