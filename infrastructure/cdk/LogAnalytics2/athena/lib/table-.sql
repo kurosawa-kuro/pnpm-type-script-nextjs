8fb1e321745bfab45000b0de04f4516a0ad97787
Toshifumi Kurosawa <kuromailserver@gmail.com>
2025-01-22 05:56:11 +0900

    Update Athena table configuration for API logs
    - Renamed external table from 'ttt02.api_logs' to 'fargate_logs_db.api_logs' to reflect the new database structure.
    - Updated S3 location for log storage from 's3://fargatestack-logbucketcc3b17e8-0djriusfgxia/' to 's3://fargatestack-logbucketcc3b17e8-0djriusfgxia/processed/' for better organization of processed logs.

--- infrastructure/cdk/LogAnalytics2/athena/lib/table-.sql
+++ infrastructure/cdk/LogAnalytics2/athena/lib/table-.sql
@@ -1,4 +1,4 @@
-CREATE EXTERNAL TABLE ttt02.api_logs (
+CREATE EXTERNAL TABLE fargate_logs_db.api_logs (
     level STRING,
     message STRING,
     method STRING,
@@ -14,6 +14,6 @@ WITH SERDEPROPERTIES (
 )
 STORED AS INPUTFORMAT 'org.apache.hadoop.mapred.TextInputFormat'
 OUTPUTFORMAT 'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
-LOCATION 's3://fargatestack-logbucketcc3b17e8-0djriusfgxia/'
+LOCATION 's3://fargatestack-logbucketcc3b17e8-0djriusfgxia/processed/'
 TBLPROPERTIES ('has_encrypted_data'='false');
 
