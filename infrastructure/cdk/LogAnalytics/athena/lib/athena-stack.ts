16f2ccd025505be2e278c069aac9cd874f487e4e
Toshifumi Kurosawa <kuromailserver@gmail.com>
2025-01-22 05:04:01 +0900

    Implement Glue Database and Table Configuration in Athena Stack
    - Added configuration constants for Glue database and table settings, including database name, description, and table schema.
    - Created a Glue database and an external table in the Athena stack, linking them to the specified S3 location for log storage.
    - Updated the environment settings in the Athena stack to use the current AWS account and region dynamically.

--- infrastructure/cdk/LogAnalytics/athena/lib/athena-stack.ts
+++ infrastructure/cdk/LogAnalytics/athena/lib/athena-stack.ts
@@ -1,16 +1,71 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as glue from 'aws-cdk-lib/aws-glue';
+
+// 設定値を定数として定義
+const CONFIG = {
+  GLUE: {
+    DATABASE_NAME: 'fargate_logs_db',  // アプリケーション_目的_db
+    DATABASE_DESCRIPTION: 'Database for Fargate application logs analysis',
+    TABLE: {
+      NAME: 'api_access_logs_raw',  // 種類_目的_状態
+      // または 'api_access_logs_partitioned' パーティション化後用
+      COLUMNS: [
+        { name: 'level', type: 'string' },
+        { name: 'message', type: 'string' },
+        { name: 'method', type: 'string' },
+        { name: 'origin', type: 'string' },
+        { name: 'pathname', type: 'string' },
+        { name: 'timestamp', type: 'string' }
+      ],
+      LOCATION: 's3://fargatestack-logbucketcc3b17e8-0djriusfgxia/'
+    }
+  }
+} as const;
 
 export class AthenaStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Glueデータベースの作成
+    const glueDatabase = new glue.CfnDatabase(this, 'GlueDB', {
+      catalogId: this.account,
+      databaseInput: {
+        name: CONFIG.GLUE.DATABASE_NAME,
+        description: CONFIG.GLUE.DATABASE_DESCRIPTION,
+      },
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AthenaQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Glueテーブルの作成
+    const glueTable = new glue.CfnTable(this, 'GlueTbl', {
+      catalogId: this.account,
+      databaseName: CONFIG.GLUE.DATABASE_NAME,
+      tableInput: {
+        name: CONFIG.GLUE.TABLE.NAME,
+        tableType: 'EXTERNAL_TABLE',
+        parameters: {
+          'has_encrypted_data': 'false',
+          'classification': 'json'
+        },
+        storageDescriptor: {
+          location: CONFIG.GLUE.TABLE.LOCATION,
+          inputFormat: 'org.apache.hadoop.mapred.TextInputFormat',
+          outputFormat: 'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat',
+          serdeInfo: {
+            serializationLibrary: 'org.openx.data.jsonserde.JsonSerDe',
+            parameters: {
+              'ignore.malformed.json': 'true',
+              'dots.in.keys': 'true',
+              'case.insensitive': 'true'
+            }
+          },
+          columns: [...CONFIG.GLUE.TABLE.COLUMNS]
+        }
+      }
+    });
+
+    // テーブルの依存関係を設定
+    glueTable.addDependency(glueDatabase);
   }
 }
